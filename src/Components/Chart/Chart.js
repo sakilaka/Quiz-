3dcc613ad7dc3edf7537e5a693cb9843635b2435
Kamrul hasan <kamrulhasan275723@gmail.com>
2022-10-12 00:00:38 +0600

    chart done

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -1,31 +1,25 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { Bar, BarChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Chart = () => {
-    const data = useLoaderData().data;
-    console.log(data);
+  const data = useLoaderData().data;
+  return (
+    <div>
+   
+     <BarChart
+        height={600}
+        width={500}
+        className="mx-auto my-8"
+        data={data}>
+          <Bar dataKey='total' fill='green'></Bar>
+          <XAxis dataKey='name' ></XAxis>
+          <YAxis></YAxis>
+          <Tooltip></Tooltip>
 
-            return (
-              <ResponsiveContainer width="100%" height="100%">
-                <AreaChart
-                  width={200}
-                  height={60}
-                  data={data}
-                  margin={{
-                    top: 5,
-                    right: 0,
-                    left: 0,
-                    bottom: 5,
-                  }}
-                >
-                    <XAxis></XAxis>
-                    <YAxis></YAxis>
-                    <Tooltip></Tooltip>
-                  <Area type="monotone" dataKey="" stroke="#8884d8" fill="#8884d8" />
-                </AreaChart>
-              </ResponsiveContainer>
-            );
+      </BarChart>
+    </div>
+  );
 };
 
 export default Chart;
