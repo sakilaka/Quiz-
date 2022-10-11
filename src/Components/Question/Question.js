bbab628b501de0b37f65914ad7ed519ac9c70fce
Kamrul hasan <kamrulhasan275723@gmail.com>
2022-10-11 12:37:09 +0600

    load quiz done

---
+++ src/Components/Question/Question.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Question = () => {
+    
+    return (
+
+        <div className='bg-lime-400 w-9/12 mx-auto p-5'>
+            <h1>QUestiom jnvjksdnjv fsdfnsdjf dfjsdnfsdjknf sdhnfj dhnfsdjn</h1>
+            <div className='bg-slate-200 my-5 w-7/12 mx-auto'>
+                <div className='p-3 flex bg-slate-400 m-3'>
+                    <input type="radio" name="" className='inline-block' id="" />
+                    <p className='ml-3'>dbvcdsbch dhfbsdhfb</p>
+                </div>
+                <div className='p-3 flex bg-slate-500 m-3'>
+                    <input type="radio" name="" className='inline-block' id="" />
+                    <p className='ml-3'>dbvcdsbch dhfbsdhfb</p>
+                </div>
+                <div className='p-3 flex bg-slate-400 m-3'>
+                    <input type="radio" name="" className='inline-block' id="" />
+                    <p className='ml-3'>dbvcdsbch dhfbsdhfb</p>
+                </div>
+                <div className='p-3 flex bg-slate-500 m-3'>
+                    <input type="radio" name="" className='inline-block' id="" />
+                    <p className='ml-3'>dbvcdsbch dhfbsdhfb</p>
+                </div>
+
+            </div>
+
+        </div>
+
+    );
+};
+
+export default Question;
--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Question from '../Question/Question';
 
 const Quiz = () => {
     const load = useLoaderData();
     const data = load.data;
-    const question = data.questions;
-    console.log(question);
+    const questions = data.questions;
+    console.log(questions);
     return (
         <div>
            <h2 className='text-3xl font-bold text-blue-500 my-4'>Quiz For {data.name}</h2>
            <div>
-                
+            {
+                questions.map(question => <Question 
+                    key={question.id}
+                    question={question}
+                ></Question>)
+            }
            </div>
         </div>
     );
