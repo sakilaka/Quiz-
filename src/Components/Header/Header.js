91e2eecbf45a863866ec223608f626a6c2955f5c
Kamrul hasan <kamrulhasan275723@gmail.com>
2022-10-12 15:15:24 +0600

    done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import './header.css';
 
 const Header = () => {
     return (
-       <div className='lg:grid lg:grid-cols-2 border style h-full p-4 m-5'>
+       <div className='lg:grid lg:grid-cols-2 style h-full p-4 m-5'>
 
        <div>
        <img src='https://i.postimg.cc/y6DjJQ13/img4.jpg' alt="" className='rounded-md w-full h-80 p-3' />
@@ -13,7 +13,7 @@ const Header = () => {
             <h1 className='text-4xl text-white font-bold text-center my-5 p-10 bg-indigo-600 '>Here is the most important quiz
             </h1>
            
-            <p className='text-xl bg-slate-200 p-5 my-3 w-10/12 mx-auto'>All of the students are attend the quiz exam and get 100000 taka.</p>
+            <p className='text-xl bg-slate-200 p-5 my-3 lg:w-10/12 mx-auto'>All of the students are attend the quiz exam and get 100000 taka.</p>
 
         </div>
        </div>
